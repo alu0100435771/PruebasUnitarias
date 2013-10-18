a7f3bded9ae1f076cb2012d4de8f456c8b9b4748
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-10-18 17:33:57 +0100

    Testeando suma

--- tc_racional.rb
+++ tc_racional.rb
@@ -1,8 +1,15 @@
-# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
-
 require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
+  def setup
+    @a = Fraccion.new(1,4)
+    @b = Fraccion.new(1,4)
+  end
+
+  def test_suma
+    r = @a.suma(@b)
+    assert r.numerador == 1 and r.denominador == 2
+  end
 end
