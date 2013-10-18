63635cae693f002268e66f776528fb61e5b6f32f
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-10-18 15:11:34 +0100

    Añadiendo funciones

--- racional.rb
+++ racional.rb
@@ -6,11 +6,11 @@ class Fraccion
 
   attr_accessor :denominador, :numerador
 
-  def inicializar
+  def initialize
     #Controlando que los numeros sean enteros, y que el denominador no sea 0
     raise ArgumentError, 'El denominador es 0' unless numerador != 0
     raise ArgumentError, 'El denominador no es entero' unless denominador.is_a? Integer
-    raise ArgumentError, 'El numerador no es entero' unless numearador.is_a? Integer
+    raise ArgumentEirror, 'El numerador no es entero' unless numearador.is_a? Integer
     
   end
 
@@ -20,25 +20,24 @@ class Fraccion
     @denominador = @denoiminador/comunDiv
   end
 
-
-  def to_s(a, b)
-  
+  def to_s
+    "#{numerador}/#{denominador}"  
   end
 
-  def suma(a, b)
-  
+  def suma(operador)
+    resultado = Fraccion.new(@numerador*operador.denominador + @denominador*operador.numerador, @denominador*operador.denominador)
   end
 
-  def resta
-
+  def resta(operador)
+    resultado = Fraccion.new(@numerador*operador.denominador - @denominador*operador.numerador, @denominador*operador.denominador)
   end
 
-  def producto
-
+  def producto(operador)
+    resultado = Fraccion.new(@numerador*operador.numerador, @denominador*operador.denominador)
   end
 
-  def division
-
+  def division(operador)
+    resultado = Fraccion.new(@numerador*operador.denominador, @denominador*operador.numerador)
   end
 
 end
