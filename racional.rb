9c245952018ba90fa3535152178a714c09422285
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-10-18 13:38:56 +0100

    Añadiendo Inicializar y Minimizar

--- racional.rb
+++ racional.rb
@@ -3,4 +3,42 @@
 require "./gcd.rb"
 
 class Fraccion
+
+  attr_accessor :denominador, :numerador
+
+  def inicializar
+    #Controlando que los numeros sean enteros, y que el denominador no sea 0
+    raise ArgumentError, 'El denominador es 0' unless numerador != 0
+    raise ArgumentError, 'El denominador no es entero' unless denominador.is_a? Integer
+    raise ArgumentError, 'El numerador no es entero' unless numearador.is_a? Integer
+    
+  end
+
+  def minimizar #Construcción de la fracción
+    comunDiv = gcd(numerador, denominador)
+    @numerador = @numerador/comunDiv
+    @denominador = @denoiminador/comunDiv
+  end
+
+
+  def to_s(a, b)
+  
+  end
+
+  def suma(a, b)
+  
+  end
+
+  def resta
+
+  end
+
+  def producto
+
+  end
+
+  def division
+
+  end
+
 end
