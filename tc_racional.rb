79ac5af354077d8b0a4f558764e0234b5d172c0d
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-10-18 17:54:52 +0100

    Mejora de testeo suma

--- tc_racional.rb
+++ tc_racional.rb
@@ -9,7 +9,14 @@ class Test_Fraccion < Test::Unit::TestCase
   end
 
   def test_suma
-    r = @a.suma(@b)
+    r = @a.suma(@b) # Probando con valores declarador en el set up 
     assert r.numerador == 1 and r.denominador == 2
+    @a.numerador = 1
+    @a.denominador = 2
+    r = @a.suma(@b) # Probando con nuevos valores para a
+    assert r.numerador == 3 and r.numerador == 3
+    @a.numerador = 0
+    r = @a.suma(@b) #Probando con numerador 0 
+    assert r.numerador == 1 and r.denominador == 4
   end
 end
